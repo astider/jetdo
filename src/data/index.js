32c63f4e29d14fafe35d990101751d019db38fc7
kkurno <kunakorn.pgm@gmail.com>
2019-05-10 01:13:49 +0700

    add jsdoc, fix param type

--- src/data/index.js
+++ src/data/index.js
@@ -6,14 +6,26 @@ class data {
     this.data = initialData;
   }
 
+  /**
+   * get
+   * @param {string} keys 'a.b.c'
+   */
   static get(keys) {
-    return utils.findDeep(keys, this.data);
+    return utils.findDeep(keys.split('.'), this.data);
   }
 
+  /**
+   * update
+   * @param {string} keys 'a.b.c'
+   * @param {*} value
+   */
   static update(keys, value) {
     this.data = utils.assocPath(keys, value, this.data);
   }
 
+  /**
+   * reset the whole data to initial
+   */
   static reset() {
     this.data = initialData;
   }
