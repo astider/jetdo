5a52806b23db7d714e03c81a2cc349faedf50895
kkurno <kunakorn.pgm@gmail.com>
2019-05-11 22:53:43 +0700

    fix data class to use init method instead constructure for create the initial data

--- src/data/index.js
+++ src/data/index.js
@@ -2,7 +2,17 @@ const initialData = require('./initialData');
 const utils = require('../utils');
 
 class data {
-  constructor() {
+  /**
+   * create the initial data
+   */
+  static init() {
+    this.data = initialData;
+  }
+
+  /**
+   * reset the whole data to initial
+   */
+  static reset() {
     this.data = initialData;
   }
 
@@ -11,6 +21,7 @@ class data {
    * @param {string} keys 'a.b.c'
    */
   static get(keys) {
+    if (this.data === undefined) throw Error('Please create the initial data by run "init" method');
     return utils.findDeep(keys.split('.'), this.data);
   }
 
@@ -20,14 +31,8 @@ class data {
    * @param {*} value
    */
   static update(keys, value) {
+    if (this.data === undefined) throw Error('Please create the initial data by run "init" method');
     this.data = utils.assocPath(keys, value, this.data);
-  }
-
-  /**
-   * reset the whole data to initial
-   */
-  static reset() {
-    this.data = initialData;
   }
 }
 
